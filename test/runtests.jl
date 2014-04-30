692ba56b7b6c8b966dbcef4bd52eab25f48e616c
Dmitry <deemonster@gmail.com>
2014-04-30 16:06:33 -0700

    Refactor tests, reuse DB creation boilerplate.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,90 +4,61 @@ using Base.Test
 # Open KC file.
 # Expected: file created
 function test_open()
-  db = Db()
-  open(db, "t.kch", KCOWRITER | KCOCREATE)
-  close(db)
+  test_with(empty_db) do db
+  end
 end
 
 # Get/Set values.
 function test_get_set()
-  db = Db()
-  open(db, "t.kch", KCOWRITER | KCOCREATE)
-
-  set(db, "a", "1")
-  set(db, "b", "2")
-
-  @assert "1" == get(db, "a")
-  @assert "2" == get(db, "b")
-
-  close(db)
+  test_with(abc_db) do db
+    @assert "1" == get(db, "a")
+    @assert "2" == get(db, "b")
+    @assert "3" == get(db, "c")
+  end
 end
 
 function test_cursor_empty()
-  db = Db()
-  open(db, tempname() * ".kch", KCOWRITER | KCOCREATE)
-
-  cur = Cursor(db)
-  @assert !_jump(cur)
-
-  close(db)
+  test_with(abc_db) do db
+    cur = Cursor(db)
+    @assert !_jump(cur)
+  end
 end
 
 function test_cursor()
-  db = Db()
-  open(db, tempname() * ".kch", KCOWRITER | KCOCREATE)
-
-  set(db, "a", "1")
-  set(db, "b", "2")
-  set(db, "c", "3")
-
-  cur = Cursor(db)
-  @assert _jump(cur)
-  @assert ("a", "1") == _current_record(cur)
-  @assert _next(cur)
-  @assert ("b", "2") == _current_record(cur)
-  @assert _next(cur)
-  @assert ("c", "3") == _current_record(cur)
-  @assert !_next(cur)
-  @test_throws _current_record(cur)
-
-  close(db)
+  test_with(abc_db) do db
+    cur = Cursor(db)
+    @assert _jump(cur)
+    @assert ("a", "1") == _current_record(cur)
+    @assert _next(cur)
+    @assert ("b", "2") == _current_record(cur)
+    @assert _next(cur)
+    @assert ("c", "3") == _current_record(cur)
+    @assert !_next(cur)
+    @test_throws _current_record(cur)
+  end
 end
 
 # Iterate through values
 function test_iterate_empty()
-  db = Db()
-  open(db, tempname() * ".kch", KCOWRITER | KCOCREATE)
-
-  cur = Cursor(db)
-  log = string()
-  for (k, v) = cur
-    log = log * " $k:$v"
+  test_with(empty_db) do db
+    cur = Cursor(db)
+    log = string()
+    for (k, v) = cur
+      log = log * " $k:$v"
+    end
+    @assert "" == log
   end
-  @assert "" == log
-
-  close(db)
 end
 
 function test_iterate()
-  db = Db()
-  open(db, tempname() * ".kch", KCOWRITER | KCOCREATE)
-
-  set(db, "a", "1")
-  set(db, "b", "2")
-  set(db, "c", "3")
-
-  cur = Cursor(db)
-  log = string()
-  for (k, v) = cur
-    log = log * " $k:$v"
+  test_with(abc_db) do db
+    cur = Cursor(db)
+    log = string()
+    for (k, v) = cur
+      log = log * " $k:$v"
+    end
+    @assert " a:1 b:2 c:3" == log
   end
-  @assert " a:1 b:2 c:3" == log
-
-  # log = join(["$k:$v" for (k, v) = cur], " ")
-  # @assert "a:1 b:2 c:3" == log
-
-  close(db)
 end
 
 function test_generator()
@@ -103,16 +74,29 @@ end
 # - Read non existing keys
 # - Set value when opened in R/O mode
 function test_get_set_failures()
-  db = Db()
-  open(db, "t.kch", KCOWRITER | KCOCREATE)
+  test_with(abc_db) do db
+    @test_throws get(db, "z")
+  end
+end
 
+function empty_db(db::Db)
+end
+
+function abc_db(db::Db)
   set(db, "a", "1")
   set(db, "b", "2")
   set(db, "c", "3")
+end
 
-  @test_throws get(db, "z")
-
-  close(db)
+function test_with(check::Function, configure::Function)
+  db = Db()
+  open(db, tempname() * ".kch", KCOWRITER | KCOCREATE)
+  try
+    configure(db)
+    check(db)
+  finally
+    close(db)
+  end
 end
 
 test_open()
